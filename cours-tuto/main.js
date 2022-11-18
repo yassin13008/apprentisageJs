320b3f742ca9e5a3bafe21fa2f991883fbedd73d
Yassin Hammami <yas.hammami@gmail.com>
2022-11-18 14:55:47 +0100

    J'ai achevé la partie : QUERY SELECTOR

--- cours-tuto/main.js
+++ cours-tuto/main.js
@@ -1,36 +1,57 @@
-let total = 0;
+// CECI EST LES PREMICES DE MON APPRENTISSAGE JS
 
-function addition (x) {
-    total += x;
-    return total;
-    }
+// let total = 0;
 
-function soustraction (x){
-    total -= x;
-    return total; }
+// function addition (x) {
+//     total += x;
+//     return total;
+//     }
 
-function division(x) {
-    if (total === 0) {
-        return total = x
-    }
-    else {
-    total /= x;
-    return total;
-    }
-} 
+// function soustraction (x){
+//     total -= x;
+//     return total; }
 
-function multiplication(x) {
-    if (total === 0) {
-        return total = x
-    }
-    else {
-    total *= x;
-    return total;
-    }
-}
+// function division(x) {
+//     if (total === 0) {
+//         return total = x
+//     }
+//     else {
+//     total /= x;
+//     return total;
+//     }
+// } 
 
-function reset () {
-    total = 0;
-}
+// function multiplication(x) {
+//     if (total === 0) {
+//         return total = x
+//     }
+//     else {
+//     total *= x;
+//     return total;
+//     }
+// }
 
-alert("ceci marche")
+// function reset () {
+//     total = 0;
+// }
+
+// alert("ceci marche")
+
+//  SELECTOR (ICI JE VAIS PARLER DES SELECTEURS )
+
+// document.querySelector('body') 
+// Ici je demande à ce que js me selectionne tout ma partie "body "
+
+// document.querySelector('.box')
+// document.querySelector('#btn-1')
+// Ca marche comme la selection des éléments CSS on peut selectionner les élements par class(.), id (#), etc... 
+// Faire un console.log de docu pour le voir apparaitre sur la console 
+//  LES METHODES = FONCTIONS QUI SONT DEJA DANS LE JAVASCRIPT EX = CONSOLE LOG
+
+// const h4 = document.querySelector('h4')
+//  Ici j'ai crée une variable et j'ai incrémenter du coup l'élément h1
+// On sait que le h4 contient la question " qu'elle est la capital de la suisse"
+//  Une fois que le doc query select est fait pas besoin de le refaire 
+
+//  CLICKS EVENTS
+
